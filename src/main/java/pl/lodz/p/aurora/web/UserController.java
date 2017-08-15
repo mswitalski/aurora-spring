c1730d878af57805cb5006cd57b712302239c8dd
Marek Świtalski <marek.switalski@outlook.com>
2017-08-15 23:46:04 +0200

    Added basic functionality for user controller and user dto implementation.
    Signed-off-by: Marek Świtalski <marek.switalski@outlook.com>

--- src/main/java/pl/lodz/p/aurora/web/UserController.java
+++ src/main/java/pl/lodz/p/aurora/web/UserController.java
@@ -1,14 +1,46 @@
 package pl.lodz.p.aurora.web;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import pl.lodz.p.aurora.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pl.lodz.p.aurora.domain.dto.UserDto;
+import pl.lodz.p.aurora.domain.entity.User;
+import pl.lodz.p.aurora.service.UserServiceImpl;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * REST controller for managing any requests related to user account.
  */
-@Controller
+@RequestMapping(value = "api/users/")
+@RestController
 public class UserController {
 
-    @Autowired private UserService userService;
+    private final UserServiceImpl userService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public UserController(UserServiceImpl userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<UserDto> findAll() {
+        return userService.findAll().stream().map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Convert given User object to UserDto.
+     *
+     * @param user Object containing data about a user
+     * @return UserDto object
+     */
+    private UserDto convertToDto(User user) {
+        return modelMapper.map(user, UserDto.class);
+    }
 }
