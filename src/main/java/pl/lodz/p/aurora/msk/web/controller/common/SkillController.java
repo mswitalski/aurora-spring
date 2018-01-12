15f85a447c73a05cfdddcad90812038708897072
Marek Świtalski <marek.switalski@outlook.com>
2018-01-12 16:31:24 +0100

    Added missing controller classes extensions
    Signed-off-by: Marek Świtalski <marek.switalski@outlook.com>

--- src/main/java/pl/lodz/p/aurora/msk/web/controller/common/SkillController.java
+++ src/main/java/pl/lodz/p/aurora/msk/web/controller/common/SkillController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.lodz.p.aurora.common.web.controller.BaseController;
 import pl.lodz.p.aurora.msk.web.converter.SkillBasicDtoConverter;
 import pl.lodz.p.aurora.msk.web.dto.SkillBasicDto;
 import pl.lodz.p.aurora.msk.service.common.SkillService;
@@ -14,7 +15,7 @@ import java.util.stream.Collectors;
 
 @RequestMapping(value = "api/v1/skills/", headers = "Requester-Role=ANY")
 @RestController
-public class SkillController {
+public class SkillController extends BaseController {
 
     private final SkillService service;
     private final SkillBasicDtoConverter converter = new SkillBasicDtoConverter();
--- src/main/java/pl/lodz/p/aurora/mus/web/controller/admin/RoleAdminController.java
+++ src/main/java/pl/lodz/p/aurora/mus/web/controller/admin/RoleAdminController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.lodz.p.aurora.common.web.controller.BaseController;
 import pl.lodz.p.aurora.mus.domain.entity.Role;
 import pl.lodz.p.aurora.mus.service.common.RoleService;
 
@@ -14,7 +15,7 @@ import java.util.List;
  */
 @RequestMapping(value = "api/v1/roles/", headers = "Requester-Role=ADMIN")
 @RestController
-public class RoleAdminController {
+public class RoleAdminController extends BaseController {
 
     private final RoleService service;
 
